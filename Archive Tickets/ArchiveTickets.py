d626e07ff09a1290b88e6ef403a57f68f88f95f3
Jack <jaxboles@gmail.com>
2020-09-21 09:19:59 -0400

    Update ArchiveTickets.py

--- Archive Tickets/ArchiveTickets.py
+++ Archive Tickets/ArchiveTickets.py
@@ -11,8 +11,8 @@ PATH = "C:\Program Files (x86)\chromedriver.exe"
 
 # Logs into Wealth
 def WealthLogin():
-    loginBox = driver.find_element_by_xpath('//*[@id="email"]').send_keys('jboles@bluelinevoip.com')
-    passBox = driver.find_element_by_xpath('//*[@id="password"]').send_keys('Hunter3259')
+    loginBox = driver.find_element_by_xpath('//*[@id="email"]').send_keys(email)
+    passBox = driver.find_element_by_xpath('//*[@id="password"]').send_keys(password)
     loginBtn = driver.find_element_by_xpath('//*[@id="submitloading"]').click()
     sleep(2)
 
